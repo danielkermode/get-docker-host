7f77c893aee17f34f6f4c3c4fa09393b3d26a55e
Dan <danielkermode@hotmail.co.nz>
2017-05-15 14:14:40 +1200

    Initial

---
+++ index.js
@@ -0,0 +1,30 @@
+const childProcess = require('child_process')
+
+const handleIpRouteResults = callback => (error, stdout, stderr) => {
+
+  if (stdout && isString(stdout)) {
+    const output = stdout
+    const match = output.match(/default via ((?:[0-9]{1,3}\.){3}[0-9]{1,3}) dev eth0/)
+
+    let ip = undefined
+    if (Array.isArray(match) && match.length >= 2) {
+      ip = match[1]
+    }
+
+    if (ip) {
+      callback(undefined, ip)
+    } else {
+      callback(new Error("Unable to find ip with /sbin/ip route|awk '/default/ { print $3 }'"), undefined)
+    }
+  } else if (error) {
+    callback(error, undefined)
+  } else if (stderr) {
+    callback(new Error(stderr), undefined)
+  } else {
+    callback(new Error('No results or feedback given'), undefined)
+  }
+}
+
+module.exports = function (callback) {
+  childProcess.exec("/sbin/ip route|awk '/default/ { print $3 }'", handleIpRouteResults(callback))
+}
